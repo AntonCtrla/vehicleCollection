ac2b3b8507a0a8ceb3bc7509556db485be2d44bc
Anton Fesenko <a.fesenko@netpeak.net>
2024-06-20 13:46:03 +0200

    added git link, added fibonacci and infinite summ function scripts

---
+++ math/fibonacci.php
@@ -0,0 +1,36 @@
+<?php
+
+// fibonacci
+function fibonacciSeq($n)
+{
+    $trace = '';
+    if ($n < 0) return;
+    $trace .= __FUNCTION__ . ': ' . PHP_EOL;
+
+    $res = [];
+    $a = 0;
+    for ($i = 0; $i < $n; $i++) {
+        $trace .= 'i[' . $i . ']';
+
+        if ($i == 0) {
+            $res[$i] = 0;
+        } else if ($i == 1) {
+            $res[$i] = 1;
+        } else $res[$i] = $res[$i - 1] + $res[$i - 2];
+
+        $trace .= '=' . $res[$i];
+        if ($i > 1) {
+            $trace .= " :: ($i-1)=" . $res[$i - 1] . " + ($i-2)=" . $res[$i - 2] . PHP_EOL;
+        } else {
+            $trace .= PHP_EOL;
+        }
+    }
+    $trace .= PHP_EOL;
+
+    echo $trace;
+
+    return array_pop($res);
+}
+
+
+echo 'Result:' . fibonacciSeq(1000);
--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,9 @@
 </div>
 
 </body>
-<footer class="text-center bg-premium">&caret;Y&caret;Y&caret;Y&caret;</footer>
+<footer class="text-center bg-premium">
+    &caret;Y&caret;Y&caret;Y&caret;<br/>
+    <a href="https://github.com/AntonCtrla/vehicleCollection" target="_blank">@GitHub</a>
+</footer>
 
 </html>
